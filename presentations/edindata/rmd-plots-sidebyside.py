60fd9e3fd11754ef202e098547cdab24c0e72bee
brochhagen <thomasbrochhagen@hhu.de>
2016-09-26 19:25:21 +0200

    new labels and style for paper plots + Added more to the analysis section

--- presentations/edindata/rmd-plots-sidebyside.py
+++ presentations/edindata/rmd-plots-sidebyside.py
@@ -15,21 +15,14 @@ import sys
 #####################
 
 
-print 'Loading data fitness'
-path =r'./results-fit' 
+print 'Loading data'
+path =r'./results1' 
 all_files = glob.glob(os.path.join(path, "*.csv"))     # advisable to use os.path.join as this makes concatenation OS independent
 df = pd.concat(pd.read_csv(f) for f in all_files)
-
-print 'Loading data learn'
-path =r'./results-learn' 
-all_files = glob.glob(os.path.join(path, "*.csv"))     # advisable to use os.path.join as this makes concatenation OS independent
-dl = pd.concat(pd.read_csv(f) for f in all_files)
 
 def cust_mean(grp):
     grp['mean'] = grp['t11_final'].mean()
     return grp
-
-
 
 
 ### development over cost ###
@@ -38,47 +31,36 @@ a = 1
 k = 5
 lam = 30
 sample = 10
+learn = 3
+
+
+final_group = df.loc[:,('alpha','prior_cost_c','lambda','k','sample_amount','learning_parameter','t9_final','t10_final','t11_final')]
+
+group = final_group.loc[final_group['alpha'] == a]
+group = group.loc[group['k'] == k]
+group = group.loc[group['lambda'] == lam]
+group = group.loc[group['sample_amount'] == sample]
+group = group.loc[group['learning_parameter'] == learn]
+
+t_final = group.groupby(['prior_cost_c'])
+t_final = t_final[['t9_final','t10_final','t11_final']].agg(np.average) 
+
+##
 learn = 1
 
-
-final_groupA = df.loc[:,'alpha':]
-final_groupB = dl.loc[:,'alpha':]
-
-groupA = final_groupA.loc[final_groupA['alpha'] == a]
-groupA = groupA.loc[groupA['k'] == k]
-groupA = groupA.loc[groupA['sample_amount'] == sample]
-groupA = groupA.loc[groupA['learning_parameter'] == learn]
-groupA = groupA.loc[groupA['prior_cost_c'] == 0.1]
-
-t_finalA = groupA.groupby(['lambda'])
-t_finalA = t_finalA[['t1_final','t2_final','t3_final','t4_final','t5_final','t6_final','t7_final','t8_final','t9_final','t10_final','t11_final','t12_final']].agg(np.average) 
-
-learn = 1
-
-groupB = final_groupB.loc[final_groupB['alpha'] == a]
+groupB = final_group.loc[final_group['alpha'] == a]
 groupB = groupB.loc[groupB['k'] == k]
 groupB = groupB.loc[groupB['lambda'] == lam]
 groupB = groupB.loc[groupB['sample_amount'] == sample]
 groupB = groupB.loc[groupB['learning_parameter'] == learn]
 
 t_finalB = groupB.groupby(['prior_cost_c'])
-t_finalB = t_finalB[['t1_final','t2_final','t3_final','t4_final','t5_final','t6_final','t7_final','t8_final','t9_final','t10_final','t11_final','t12_final']].agg(np.average) 
-
-#ax = t_finalB.plot(title=r'($\alpha = %d, \lambda = %d, k = %d$, samples = %d, l =%d)' %(a,lam,k,sample,learn))
-#ax.set(ylabel="Proportion in population",xlabel='Prior parameter c')
-#plt.legend(["$L1$", "$L2$", "$L3$", "$L4$","$L5$", "$L6$"], loc='best')
-
-d1 = t_finalA.loc[:,('t1_final','t2_final','t3_final', 't4_final', 't5_final', 't6_final')]
-d2 = t_finalA.loc[:,('t9_final','t10_final', 't11_final')]
-d3 = t_finalB.loc[:,('t1_final','t2_final','t3_final', 't4_final', 't5_final', 't6_final')]
-d4 = t_finalB.loc[:,('t9_final','t10_final', 't11_final')]
-
+t_finalB = t_finalB[['t9_final','t10_final','t11_final']].agg(np.average) 
 
 sns.set(font_scale=1.2) 
-
 fig = plt.figure()
 
-ax1 = fig.add_subplot(1,2,1, xlabel=r'Rationality parameter $\lambda$', ylabel='Proportion in population')
+ax1 = fig.add_subplot(1,2,1, xlabel=r'Prior parameter c', ylabel='Proportion in population')
 ax1.set_title("A", x=0.5, y=0.92, fontsize=20, fontweight='bold')
 
 ax2 = fig.add_subplot(1,2,2, sharey = ax1, xlabel='Prior parameter c')  #Share y-axes with subplot 1
@@ -87,11 +69,9 @@ ax2.set_title("B", x=0.5, y=0.92, fontsize=20, fontweight='bold')
 plt.setp(ax2.get_yticklabels())#, visible=False)
 plt.tight_layout(pad=0.4, w_pad=0.5, h_pad=0.5)
 
-
-
 #Plot data
-im1 = ax1.plot(d2)
-im2 = ax2.plot(d4)
+im1 = ax1.plot(t_finalB)
+im2 = ax2.plot(t_final)
 plt.legend(["pragmatic L3", "pragmatic L4","pragmatic L5"], loc='best', prop={'size':11})
 
 
@@ -104,3 +84,9 @@ plt.legend(["pragmatic L3", "pragmatic L4","pragmatic L5"], loc='best', prop={'s
 #plt.legend(["pragmatic L-taut", "pragmatic L-bound","pragmatic L-lack"], loc='best')
 #plt.xlim(0,48)
 plt.show() 
+
+
+
+plt.show()
+
+
---
+++ presentations/edindata/tabular-info.py
@@ -0,0 +1,90 @@
+###
+import pandas as pd
+import numpy as np 
+import os
+import glob
+import sys
+###
+###Column headers:###
+#run_ID,t1_initial,t2_initial,t3_initial,t4_initial,t5_initial,t6_initial,t7_initial,t8_initial,t9_initial,t10_initial,t11_initial,t12_initial,
+#alpha,prior_cost_c,lambda,k,sample_amount,learning_parameter,generations,
+#t1_final,t2_final,t3_final,t4_final,t5_final,t6_final,t7_final,t8_final,t9_final,t10_final,t11_final,t12_final
+#####################
+
+
+print 'Loading data'
+path =r'./results1' 
+all_files = glob.glob(os.path.join(path, "*.csv"))     # advisable to use os.path.join as this makes concatenation OS independent
+df = pd.concat(pd.read_csv(f) for f in all_files)
+
+def cust_mean(grp):
+    grp['mean'] = grp['t11_final'].mean()
+    return grp
+
+### development over cost ###
+
+a = 1
+k = 5
+lam = 30
+sample = 10
+
+
+final_group = df.loc[:,('alpha','prior_cost_c','lambda','k','sample_amount','learning_parameter','t1_final','t2_final', 't3_final', 't4_final', 't5_final', 't6_final', 't7_final', 't8_final', 't9_final','t10_final','t11_final', 't12_final')]
+
+group = final_group.loc[final_group['alpha'] == a]
+group = group.loc[group['k'] == k]
+group = group.loc[group['lambda'] == lam]
+group = group.loc[group['sample_amount'] == sample]
+group1 = group.loc[group['learning_parameter'] == 1]
+
+group1 = group1.groupby(['prior_cost_c']).mean()
+
+group2 = group.loc[group['learning_parameter'] == 3]
+group2 = group2.groupby(['prior_cost_c']).mean()
+
+sys.exit()
+##
+learn = 1
+
+groupB = final_group.loc[final_group['alpha'] == a]
+groupB = groupB.loc[groupB['k'] == k]
+groupB = groupB.loc[groupB['lambda'] == lam]
+groupB = groupB.loc[groupB['sample_amount'] == sample]
+groupB = groupB.loc[groupB['learning_parameter'] == learn]
+
+t_finalB = groupB.groupby(['prior_cost_c'])
+t_finalB = t_finalB[['t9_final','t10_final','t11_final']].agg(np.average) 
+
+sns.set(font_scale=1.2) 
+fig = plt.figure()
+
+ax1 = fig.add_subplot(1,2,1, xlabel=r'Prior parameter c', ylabel='Proportion in population')
+ax1.set_title("A", x=0.5, y=0.92, fontsize=20, fontweight='bold')
+
+ax2 = fig.add_subplot(1,2,2, sharey = ax1, xlabel='Prior parameter c')  #Share y-axes with subplot 1
+ax2.set_title("B", x=0.5, y=0.92, fontsize=20, fontweight='bold')
+
+plt.setp(ax2.get_yticklabels())#, visible=False)
+plt.tight_layout(pad=0.4, w_pad=0.5, h_pad=0.5)
+
+#Plot data
+im1 = ax1.plot(t_finalB)
+im2 = ax2.plot(t_final)
+plt.legend(["pragmatic L3", "pragmatic L4","pragmatic L5"], loc='best', prop={'size':11})
+
+
+#ax2 = plt.plot(d2)
+##ax1 = plt.plot(d1,marker='d',markevery=5)
+#plt.title(r'($\alpha$ = %d)' %(a))
+#plt.ylabel("Proportion in population")
+#plt.xlabel(r'Rationality parameter $\lambda$')
+#
+#plt.legend(["pragmatic L-taut", "pragmatic L-bound","pragmatic L-lack"], loc='best')
+#plt.xlim(0,48)
+plt.show() 
+
+
+
+plt.show()
+
+
